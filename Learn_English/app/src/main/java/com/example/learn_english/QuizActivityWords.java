587e5a171c5a9b723b2bb10dbaa7ce2e30df18a2
SofiMovsesyan <sofi170707@gmail.com>
2023-05-30 22:33:43 +0400

    Words functionality completed(img with firebase)

--- Learn_English/app/src/main/java/com/example/learn_english/QuizActivityWords.java
+++ Learn_English/app/src/main/java/com/example/learn_english/QuizActivityWords.java
@@ -15,11 +15,11 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.util.IOUtils;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -44,7 +44,7 @@ public class QuizActivityWords extends AppCompatActivity {
     final List<QuestionsList> questionsLists = new ArrayList<>();
     private String getSelectedTopicName;
     private int rightAnswers = 0;
-
+    private ImageView imageView;
 
     private int curQuestPos = 0;
     private String selectedOptionByUser = "";
@@ -64,15 +64,16 @@ public class QuizActivityWords extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar2);
         check = findViewById(R.id.check);
         selectedTopicName.setText(getSelectedTopicName);
+        imageView = findViewById(R.id.imageView);
 
 
         loadQuestions();
 
 
         check.setOnClickListener(v -> {
-                selectedOptionByUser = word.getText().toString().toLowerCase();
-                revealAnswer();
-                questionsLists.get(curQuestPos).setUserSelected(selectedOptionByUser);
+            selectedOptionByUser = word.getText().toString().toLowerCase();
+            revealAnswer();
+            questionsLists.get(curQuestPos).setUserSelected(selectedOptionByUser);
         });
 
         nextBtn.setOnClickListener(v -> {
@@ -83,6 +84,7 @@ public class QuizActivityWords extends AppCompatActivity {
             }
         });
     }
+
     private boolean flag = false;
 
     private void changeNextQuestion() {
@@ -129,6 +131,7 @@ public class QuizActivityWords extends AppCompatActivity {
 
 
     }
+
     private int getCorrectAnswers() {
         int correctAnswers = 0;
         for (int i = 0; i < questionsLists.size(); i++) {
@@ -163,11 +166,21 @@ public class QuizActivityWords extends AppCompatActivity {
         if (word.getText().toString().toLowerCase().equals(getAnswer)) {
             word.setTextColor(Color.parseColor("#62B865"));
             word.setEnabled(false);
-//            word.setFocusableInTouchMode(false);
-//            word.setFocusable(false);
-//            word.setBackgroundColor(R.color.my_green);
-        }
-        else {
+           /* // image to firebase
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.words);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] imageBytes = baos.toByteArray();
+
+            String base64Image = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+            // Save the Base64 image string to Firebase Realtime Database
+            DatabaseReference imageRef = FirebaseDatabase.getInstance().getReference().child("words")
+                    .child("quiz").child(getSelectedTopicName).child("question1");
+            imageRef.child("image").setValue(base64Image);
+            Toast.makeText(this, "Image saved to Firebase!", Toast.LENGTH_SHORT).show();
+*/
+        } else {
             word.setTextColor(Color.parseColor("#ff0000"));
             word.setText(getAnswer);
             word.setEnabled(false);
@@ -179,6 +192,90 @@ public class QuizActivityWords extends AppCompatActivity {
 
     }
 
+    //    private class LoadQuestionsTask extends AsyncTask<Void, Void, List<QuestionsList>> {
+//
+//        @Override
+//        protected List<QuestionsList> doInBackground(Void... voids) {
+//
+//            question.setVisibility(View.GONE);
+//            questions.setVisibility(View.GONE);
+//            word.setVisibility(View.GONE);
+//            check.setVisibility(View.GONE);
+////            option1.setVisibility(View.GONE);
+////            option2.setVisibility(View.GONE);
+////            option3.setVisibility(View.GONE);
+////            option4.setVisibility(View.GONE);
+//            nextBtn.setVisibility(View.GONE);
+//
+//
+//            DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("words").child("quiz").child(getSelectedTopicName);
+//
+//            final CountDownLatch latch = new CountDownLatch(1);
+//            firebaseDatabase.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                    questionsLists.clear();
+//                    for (DataSnapshot child : snapshot.getChildren()) {
+//                        QuestionsList questionsList1 = child.getValue(QuestionsList.class);
+//                        if (questionsList1 != null) {
+//                            questionsLists.add(questionsList1);
+//                        }
+//                    }
+//                    latch.countDown();
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError error) {
+//                    questionsLists.add(new QuestionsList("q", "1", "2", "3", "4", "1"));
+//                    latch.countDown();
+//                }
+//            });
+//
+//            try {
+//                latch.await();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return questionsLists;
+//        }
+//
+//
+//
+//        @Override
+//        protected void onPostExecute(List<QuestionsList> questionsLists) {
+//            question.setVisibility(View.VISIBLE);
+//            questions.setVisibility(View.VISIBLE);
+//            word.setVisibility(View.VISIBLE);
+//            check.setVisibility(View.VISIBLE);
+//            nextBtn.setVisibility(View.VISIBLE);
+//            progressBar.setVisibility(View.GONE);
+//
+//            questions.setText((curQuestPos + 1) + "/" + questionsLists.size());
+//            question.setText(questionsLists.get(0).getQuestion());
+//            DatabaseReference imageRef = FirebaseDatabase.getInstance().getReference()
+//                    .child("words").child("quiz").child(getSelectedTopicName).child("question1").child("image");
+////            imageRef.addValueEventListener(new ValueEventListener() {
+////                                               @Override
+////                                               public void onDataChange(@NonNull DataSnapshot snapshot) {
+////                                                   String base64Image = snapshot.getValue(String.class);
+////                                                   if (base64Image != null) {
+////                                                       byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+////                                                       Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+////                                                       imageView.setImageBitmap(bitmap);
+////                                                   }
+////                                               }
+//                                           }
+////
+//////            option4.setText(questionsLists.get(0).getOption4());
+////
+//    }
+//
+//
+//            private void loadQuestions () {
+//                new LoadQuestionsTask().execute();
+//            }
+//        }
     private class LoadQuestionsTask extends AsyncTask<Void, Void, List<QuestionsList>> {
 
         @Override
@@ -188,12 +285,7 @@ public class QuizActivityWords extends AppCompatActivity {
             questions.setVisibility(View.GONE);
             word.setVisibility(View.GONE);
             check.setVisibility(View.GONE);
-//            option1.setVisibility(View.GONE);
-//            option2.setVisibility(View.GONE);
-//            option3.setVisibility(View.GONE);
-//            option4.setVisibility(View.GONE);
             nextBtn.setVisibility(View.GONE);
-
 
             DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("words").child("quiz").child(getSelectedTopicName);
 
@@ -238,8 +330,28 @@ public class QuizActivityWords extends AppCompatActivity {
 
             questions.setText((curQuestPos + 1) + "/" + questionsLists.size());
             question.setText(questionsLists.get(0).getQuestion());
+            DatabaseReference imageRef = FirebaseDatabase.getInstance().getReference()
+                    .child("words").child("quiz").child(getSelectedTopicName).child("question1").child("image");
+            imageRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String base64Image = snapshot.getValue(String.class);
+                    if (base64Image != null) {
+                        byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
 
-//            option4.setText(questionsLists.get(0).getOption4());
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Handle any errors or interruptions
+                }
+            });
+
+            // Update the UI with the loaded questions and options
+            // ...
+
         }
     }
 
